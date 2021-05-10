8ab0bcb128d44b910cdcec4e719840def48ade10
Samuli Löf <samuli.lof@gmail.com>
2021-05-10 21:54:21 +0300

    Fix package.json. Logging in broadcastServer

--- src/telegram/broadcastServer.ts
+++ src/telegram/broadcastServer.ts
@@ -1,12 +1,13 @@
-// Logger depends on this so can't use here
-/* eslint-disable no-console */
 import { injectable } from "inversify";
 import { Telegraf } from "telegraf";
 import { utils } from "../utils";
 import { bot_token } from "./bot_token";
 import { BroadcastFileService } from "./broadcastFileService";
 
-const samuli_telegram_id = "72781909";
+function log(msg: string) {
+  // eslint-disable-next-line no-console
+  console.log(utils.getTimestamp() + msg);
+}
 
 @injectable()
 export class BroadcastServer {
@@ -15,31 +16,29 @@ export class BroadcastServer {
 
     bot.start((ctx) => {
       this.addToBroadcastlist(ctx.from.id);
+      log("/add called");
       ctx.reply(
         "You have been added to list. You can stop your subscription with command /quit. I will notify you if I find seats available"
       );
     });
     bot.command("/quit", (ctx) => {
       this.removeFromBroadcastlist(ctx.from.id);
+      log("/quit called");
       ctx.reply(
         "You have been removed from list. To rejoin you can send /start"
       );
     });
-    bot.command("/sendbroadcast", (ctx) => {
-      if (ctx.from.id.toString() !== samuli_telegram_id) return;
-      //this.sendBroadcast(ctx.message.text.replace("/sendbroadcast", "").trim());
-    });
 
-    console.log(`${utils.getTimestamp()} Starting telegraf bot`);
+    log("Starting telegraf bot");
     bot.launch();
 
     // Enable graceful stop
     process.once("SIGINT", () => {
-      console.log("SIGINT to telegraf bot");
+      log("SIGINT to telegraf bot");
       bot.stop("SIGINT");
     });
     process.once("SIGTERM", () => {
-      console.log("SIGTERM to telegraf bot");
+      log("SIGTERM to telegraf bot");
       bot.stop("SIGTERM");
     });
   }
