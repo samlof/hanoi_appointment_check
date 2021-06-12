21700d93c94ac8f32313181b7669a8b27d9757a2
Samuli Löf <samuli.lof@gmail.com>
2021-06-12 18:45:27 +0300

    Add logged out check so it won't be error

--- src/main.ts
+++ src/main.ts
@@ -132,7 +132,10 @@ async function checkSeatsCalendar(
       let stack: string = "";
       if (error.stack && typeof error.stack === "string") stack = error.stack;
       const searchString = error.message + stack;
-      if (searchString.includes("Invalid url")) {
+
+      if (searchString.includes("?ReturnUrl=/")) {
+        // Was logged out. Can just continue
+      } else if (searchString.includes("Invalid url")) {
         // Invalid url
         logger.log(
           `Invalid url. Got ${categoryName} url ${page.url()} and error ${
